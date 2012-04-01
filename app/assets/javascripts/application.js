3909f09456f86b23b50a2600a07c1f273455f879
Jason Zhao <jasonzhao6@gmail.com>
2012-03-31 17:52:05 -0700

    pano prototype

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,3 +7,34 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+function addOne(lat, lng, heading, pitch, zoom) {
+  lat = parseFloat(lat);
+  lng = parseFloat(lng);
+  heading = parseFloat(heading);
+  pitch = parseFloat(pitch);
+  zoom = parseFloat(zoom);
+  var latLng = new google.maps.LatLng(lat, lng);
+  var mapOptions = {
+    center: latLng,
+    zoom: 5,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(
+    document.getElementById('map'), mapOptions
+  );
+  var panoramaOptions = {
+    position: latLng,
+    pov: {
+      heading: heading,
+      pitch: pitch,
+      zoom: zoom
+    }
+  };
+  var panorama = new google.maps.StreetViewPanorama(document.getElementById('pano'), panoramaOptions);
+  map.setStreetView(panorama);
+}
+
+$(function() {
+  addOne("36.926679", "14.735641", "111.17", "1.76", "0");
+});
